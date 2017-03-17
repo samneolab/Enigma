2c1cc6c3a66896ca02b0ddbb4720afcce14a6ed8
LongHV <long.hv@neo-lab.vn>
2017-03-17 14:35:29 +0700

    define constant for api

--- Enigma/app/src/main/java/com/neolab/enigma/ws/ApiService.java
+++ Enigma/app/src/main/java/com/neolab/enigma/ws/ApiService.java
@@ -40,92 +40,113 @@ import retrofit.http.QueryMap;
  */
 public interface ApiService {
 
-
-    static final String GET_CAPTCHA_IMAGE = "/captcha-images";
+    String GET_CAPTCHA_IMAGE = "/captcha-images";
+    String LOGIN = "/authenticate";
+    String GET_EMERGENCY_ANNOUNCEMENT = "/announcements/emergency";
+    String GET_ANNOUNCEMENT_LIST = "/announcements?per_page=3&publish_type=open";
+    String GET_MONEY_AVAILABLE_FOR_PREPAYMENT = "/payment-request/available-payment";
+    String GET_ANNOUNCEMENT_DETAIL = "/announcements/{id}";
+    String GET_MAX_MONEY_PREPAYMENT = "/payment-request/fee-setting";
+    String VALIDATE_MONEY_PREPAYMENT = "/payment-request/validate";
+    String PAYMENT_REQUEST = "/payment-request";
+    String GET_HISTORY_RECENT_PAYMENT_OF_MONTH = "/payment-request";
+    String GET_DETAIL_PAYMENT = "/payment-request/{id}";
+    String CANCEL_PAYMENT_REQUEST = "/payment-requests/{id}/status/3";
+    String GET_MONTH_REQUEST_PAYMENT_LIST = "/payment-request/month";
+    String GET_HISTORY_PAYMENT_OF_MONTH_LIST = "/payment-request";
+    String GET_USER_INFORMATION = "/employee";
+    String UPDATE_USER_INFORMATION = "/employee";
+    String STOP_USING_SERVICE = "/employee/stop-service";
+    String AGREE_USING_SERVICE = "/employee/agree";
+    String CONFIRM_ACCOUNT_INFORMATION = "/employee/confirm";
+    String LOGOUT = "/logout";
+    String RESET_PASSWORD_VIA_EMAIL = "/password/email";
+    String RESET_PASSWORD_VIA_PHONE = "/password/forgot";
+    String GET_TERM_AND_CONDITION = "/employee/agree";
 
     @FormUrlEncoded
-    @POST("/authenticate")
+    @POST(LOGIN)
     void login(@Field(ApiParameter.COMPANY_CODE) String companyCode,
                @Field(ApiParameter.EMPLOYEE_CODE) String employeeCode,
                @Field(ApiParameter.EMPLOYEE_PASSWORD) String employeePassword,
                ApiCallback<LoginResponse> callback);
 
-    @GET("/announcements/emergency")
+    @GET(GET_EMERGENCY_ANNOUNCEMENT)
     void getEmergencyAnnouncement(ApiCallback<EmergencyAnnouncementResponse> callback);
 
-    @GET("/announcements?per_page=3&publish_type=open")
+    @GET(GET_ANNOUNCEMENT_LIST)
     void getAnnouncementList(@QueryMap Map<String, String> params, ApiCallback<AnnouncementResponse> callback);
 
-    @GET("/payment-request/available-payment")
+    @GET(GET_MONEY_AVAILABLE_FOR_PREPAYMENT)
     void getMoneyAvailableForPrepayment(ApiCallback<MoneyPrepaymentResponse> callback);
 
-    @GET("/announcements/{id}")
+    @GET(GET_ANNOUNCEMENT_DETAIL)
     void getAnnouncementDetail(@Path("id") int id, ApiCallback<AnnouncementDetailResponse> callback);
 
-    @GET("/payment-request/fee-setting")
+    @GET(GET_MAX_MONEY_PREPAYMENT)
     void getMaxMoneyPrepayment(ApiCallback<FeeResponse> callback);
 
     @FormUrlEncoded
-    @POST("/payment-request/validate")
+    @POST(VALIDATE_MONEY_PREPAYMENT)
     void validateMoneyPrepayment(@Field(ApiParameter.AMOUNT_OF_SALARY) int amountOfSalary, ApiCallback<ValidateMoneyPaymentResponse> callback);
 
     @FormUrlEncoded
-    @POST("/payment-request")
+    @POST(PAYMENT_REQUEST)
     void paymentRequest(@Field(ApiParameter.AMOUNT_OF_SALARY) int amountOfSalary, @Field(ApiParameter.CAPTCHA) String captcha,
                         @Field(ApiParameter.CAPCHA_ID) String captchaId, ApiCallback<PaymentRequestResponse> callback);
 
-    @GET("/payment-request")
+    @GET(GET_HISTORY_RECENT_PAYMENT_OF_MONTH)
     void getHistoryRecentPaymentOfMonth(ApiCallback<HistoryThisMonthResponse> callback);
 
-    @GET("/payment-request/{id}")
+    @GET(GET_DETAIL_PAYMENT)
     void getDetailPayment(@Path("id") int id, ApiCallback<DetailPaymentResponse> callback);
 
-    @PUT("/payment-requests/{id}/status/3")
+    @PUT(CANCEL_PAYMENT_REQUEST)
     void cancelPaymentRequest(@Path("id") int id, ApiCallback<CancelPaymentResponse> callback);
 
-    @GET("/payment-request/month")
+    @GET(GET_MONTH_REQUEST_PAYMENT_LIST)
     void getMonthRequestPaymentList(ApiCallback<MonthPaymentResponse> callback);
 
-    @GET("/payment-request")
+    @GET(GET_HISTORY_PAYMENT_OF_MONTH_LIST)
     void getListHistoryPaymentOfMonth(@QueryMap Map<String, Integer> value, ApiCallback<HistoryThisMonthResponse> callback);
 
-    @GET("/employee")
+    @GET(GET_USER_INFORMATION)
     void getUserInformation(ApiCallback<UserInformationResponse> callback);
 
-    @PUT("/employee")
+    @PUT(UPDATE_USER_INFORMATION)
     @FormUrlEncoded
     void updateUserInformation(@Field(ApiParameter.EMAIL) String email, @Field(ApiParameter.IS_GETTING_ANNOUNCEMENT) int isGettingAnnouncement,
                                @Field(ApiParameter.CURRENT_PASSWORD) String currentPassword, ApiCallback<UserUpdateInforResponse> callback);
 
     @FormUrlEncoded
-    @POST("/employee/stop-service")
+    @POST(STOP_USING_SERVICE)
     void stopUsingService(@Field(ApiParameter.PASSWORD) String password, ApiCallback<StopServiceResponse> callback);
 
     @FormUrlEncoded
-    @POST("/employee/agree")
+    @POST(AGREE_USING_SERVICE)
     void agreeUsingService(@Field(ApiParameter.AGREE) int agree, ApiCallback<UserAgreeTermResponse> callback);
 
     @FormUrlEncoded
-    @POST("/employee/confirm")
+    @POST(CONFIRM_ACCOUNT_INFORMATION)
     void confirmAccountInformation(@Field(ApiParameter.EMAIL) String email, @Field(ApiParameter.PASSWORD) String password,
                                    @Field(ApiParameter.PASSWORD_CONFIRM) String passwordConfirm, @Field(ApiParameter.NAME) String name,
                                    ApiCallback<UserChangePasswordResponse> callback);
 
-    @GET("/logout")
+    @GET(LOGOUT)
     void logout(ApiCallback<UserLogoutResponse> callback);
 
     @FormUrlEncoded
-    @POST("/password/email")
+    @POST(RESET_PASSWORD_VIA_EMAIL)
     void resetPasswordViaMail(@Field(ApiParameter.EMAIL) String email, @Field(ApiParameter.COMPANY_CODE) String companyCode,
                                    ApiCallback<ApiResponse> callback);
 
     @FormUrlEncoded
-    @POST("/password/forgot")
+    @POST(RESET_PASSWORD_VIA_PHONE)
     void resetPasswordViaPhone(@Field(ApiParameter.COMPANY_CODE) String companyCode, @Field(ApiParameter.EMPLOYEE_CODE) String employeeCode,
                                @Field(ApiParameter.EMPLOYEE_NAME) String employeeName, @Field(ApiParameter.EMPLOYEE_PHONE) String phoneNumber,
                                 ApiCallback<ApiResponse> callback);
 
-    @GET("/employee/agree")
+    @GET(GET_TERM_AND_CONDITION)
     void getTermAndCondition(ApiCallback<TermUsingServiceResponse> callback);
 
     @GET(GET_CAPTCHA_IMAGE)
